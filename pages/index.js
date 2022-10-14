6434301547a0d27a17864a51474e1bcdb426c737
cr34t1ve <mrsofua@gmail.com>
2022-10-14 17:44:40 +0000

    chore: add meta

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,19 @@ export default function Home() {
     <Wrapper>
       <Head>
         <title>Master Sofua Desmond</title>
-        <meta name="Sofua" content="Desmond Sofua" />
         <link rel="icon" href="/favicon.png" />
+        <meta name="theme-color" content="#111111" />
+        <meta property="og:title" content="Desmond Sofua" />
+        <meta name="description" content="Desmond Sofua"></meta>
+        <meta
+          property="og:image"
+          content="https://uploads-ssl.webflow.com/625def3e532869b398ef78cf/62a8e8a6a9d847cdf54f4ef4_OpenGraph.png"
+        />
+        <meta
+          name="keywords"
+          content="Desmond, Sofua, Senanu, Desmond Sofua"
+        ></meta>
+        <meta name="author" content="Desmond Sofua"></meta>
       </Head>
       <LottieWrapper>
         <Lottie
