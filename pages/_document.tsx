6ba0e831d2575fb3d2ead65367d908ead2c96247
cr34t1ve <mrsofua@gmail.com>
2022-12-31 00:51:21 +0000

    feat: add ssr

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+/** @format */
+
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "../stitches.config";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+          <link
+            rel="preload"
+            href="/fonts/Inter-variable-optimized.woff2"
+            as="font"
+            crossOrigin=""
+            type="font/woff2"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
