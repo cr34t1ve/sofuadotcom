bb12981ed049dc7334f53d57510ad2327662bfe5
cr34t1ve <mrsofua@gmail.com>
2023-04-20 14:44:45 +0000

    chore: add redirect middleware

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+// middleware.ts
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.rewrite(new URL("/portfolio", request.url));
+  }
+}
